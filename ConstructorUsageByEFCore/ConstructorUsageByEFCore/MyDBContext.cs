5738111967cfa1d0153fe508ffa35b93a250540d
kicaj29 <kicaj29@wp.pl>
2021-04-01 08:32:08 +0200

    ef core constructor usage

---
+++ ConstructorUsageByEFCore/ConstructorUsageByEFCore/MyDBContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorUsageByEFCore
+{
+    public class MyDBContext : DbContext
+    {
+        public virtual DbSet<Person> Persons { get; set; }
+
+        public MyDBContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+        }
+
+        private static DbContextOptions GetOptions(string connectionString)
+        {
+            
+        }
+    }
+}
---
+++ ConstructorUsageByEFCore/ConstructorUsageByEFCore/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorUsageByEFCore
+{
+    public class Person
+    {
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string SecondName { get; private set; }
+        public string Address { get; private set; }
+    }
+}
